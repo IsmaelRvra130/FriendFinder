cf09e17cc09f89baec0467eefd7ec689e8c8be4a
Ismael Rivera <i.rvra130@gmail.com>
2019-05-21 22:35:31 -0400

    updated on server.js/routes.js

---
+++ server.js
@@ -0,0 +1,16 @@
+//Dependencies
+const express = require("express");
+const path = require("path");
+
+//Sets up express App
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//required html file routes.
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+//Server begins to listen
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
