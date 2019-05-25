f7daae14f250a3891a92d9ebc4921f49017e7582
Ismael Rivera <i.rvra130@gmail.com>
2019-05-25 02:16:31 -0400

    Finished product/ everything working, wish to add css at another time

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,11 @@
 
 
 // require the data of the array from friends.js file when made.
-var friends = require('../data/friends');
+var friendsList = require('../data/friends');
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
-        res.json(friends);
+        res.json(friendsList);
     });
 
     app.post("/api/friends",function(req, res){
@@ -26,28 +26,29 @@ module.exports = function(app) {
         var totalDifference = 0;
 
         //Loop through the friends array.
-        for (var i = 0; i < friends.length; i++) {
-            console.log(friends[i]);
+        for (var i = 0; i < friendsList.length; i++) {
+            //console.log(friends[i]);
             totalDifference = 0;
 
             //Loops through friends scores.
-            for (var j = 0; j < friends[i].score[j]; j++) {
+            for (var j = 0; j < friendsList[i].scores[j]; j++) {
 
                 //Calculates the difference between the sores and sums them into totalDifference.
-                 totalDifference += Math.abs(parseInt(userScores[j]) = parseInt(friends[i].scores[j]));
+                 totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendsList[i].scores[j]));
 
                  //If the sum of the differences is less then differences of the current "best match".
                  if (totalDifference <= bestMatch.friendDifference) {
 
                     //Reset the bestmatch to be the new friend.
-                    bestMatch.name = friend[i].name;
-                    bestMatch.photo = friends[i].photo;
+                    bestMatch.name = friendsList[i].name;
+                    bestMatch.photo = friendsList[i].photo;
                     bestMatch.friendDifference = totalDifference;
                  }
             }
         }
         //Save the user Data to the database.
-        friends.push(userData);
+         // User
+        friendsList.push(userData);
 
         //returns a json with the users bestmatch.
         res.json(bestMatch);
